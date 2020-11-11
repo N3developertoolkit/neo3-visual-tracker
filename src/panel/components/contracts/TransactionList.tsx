441d477e72b7271229c433e5647fdb47d55721d0
David Nicholson <me@djnicholson.com>
2020-11-10 18:17:03 -0800

    stop showing JSON in tx list

--- src/panel/components/contracts/TransactionList.tsx
+++ src/panel/components/contracts/TransactionList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Hash from "../Hash";
 import RecentTransaction from "../../../shared/recentTransaction";
 
 type Props = {
@@ -17,9 +18,48 @@ export default function TransactionList({ transactions }: Props) {
       >
         TRANSACTIONS
       </div>
-      {!!transactions.length && (
-        <pre>{JSON.stringify(transactions, undefined, 2)}</pre>
-      )}
+      {transactions.map((entry) => (
+        <div
+          style={{
+            marginBottom: 10,
+            backgroundColor: "var(--vscode-editorWidget-background)",
+            color: "var(--vscode-editorWidget-foreground)",
+            border: "var(--vscode-editorWidget-border)",
+            borderRadius: 10,
+            padding: 10,
+            cursor: entry.tx ? "pointer" : undefined,
+          }}
+          onClick={entry.tx ? () => {} : undefined}
+        >
+          <div
+            style={{
+              color: "var(--vscode-panelTitle-inactiveForeground)",
+              fontWeight: "bold",
+            }}
+          >
+            {entry.blockchain}
+            <span
+              style={{
+                float: "right",
+                color:
+                  entry.state === "error"
+                    ? "var(--vscode-errorForeground)"
+                    : undefined,
+              }}
+            >
+              {entry.state}
+            </span>
+          </div>
+          <div
+            style={{
+              paddingTop: 10,
+              fontSize: "1.1rem",
+            }}
+          >
+            <Hash hash={entry.txid} />
+          </div>
+        </div>
+      ))}
       {!transactions.length && (
         <>As you run the steps in your file, results will appear here.</>
       )}
