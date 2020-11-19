9230261d35d7ab099394e3255544e345090736db
David Nicholson <me@djnicholson.com>
2020-11-19 14:15:01 -0800

    manifests for well-known contracts should be available even when not connected to a blockchain. fixes #5

--- src/extension/autoComplete.ts
+++ src/extension/autoComplete.ts
@@ -1,4 +1,7 @@
+import * as fs from "fs";
 import * as path from "path";
+import * as temp from "temp";
+import { ContractManifestJson } from "@cityofzion/neon-core/lib/sc";
 
 import ActiveConnection from "./activeConnection";
 import AutoCompleteData from "../shared/autoCompleteData";
@@ -22,6 +25,10 @@ export default class AutoComplete {
   private disposed = false;
   private latestData: AutoCompleteData;
 
+  private readonly wellKnownManifests: {
+    [contractHash: string]: Partial<ContractManifestJson>;
+  } = {};
+
   get data() {
     return this.latestData;
   }
@@ -42,10 +49,74 @@ export default class AutoComplete {
       addressNames: {},
     };
     this.refreshLoop();
+    this.initializeWellKnownManifests();
   }
 
   dispose() {
     this.disposed = true;
+  }
+
+  private async initializeWellKnownManifests() {
+    const tempFile = temp.openSync();
+    try {
+      fs.closeSync(tempFile.fd);
+      fs.unlinkSync(tempFile.path);
+      const result = this.neoExpress.runSync(
+        "create",
+        "-f",
+        "-c",
+        "1",
+        tempFile.path
+      );
+      if (result.isError) {
+        console.error(
+          LOG_PREFIX,
+          "Could not create temporary neo-express instance, built-in contract manifests will be unavailable",
+          result.message
+        );
+      } else {
+        for (const hash of Object.keys(WELL_KNOWN_NAMES)) {
+          const contractQuery = this.neoExpress.runSync(
+            "contract",
+            "get",
+            hash,
+            "-i",
+            tempFile.path
+          );
+          if (contractQuery.isError) {
+            console.error(
+              LOG_PREFIX,
+              "Could not get manifest from neo-express for built-in contract",
+              hash,
+              result.message
+            );
+          } else {
+            try {
+              this.wellKnownManifests[hash] = JSON.parse(
+                contractQuery.message
+              ) as Partial<ContractManifestJson>;
+            } catch (e) {
+              console.error(
+                LOG_PREFIX,
+                "Could not parse manifest from neo-express for built-in contract",
+                hash,
+                e.message
+              );
+            }
+          }
+        }
+      }
+    } catch (e) {
+      console.error(
+        LOG_PREFIX,
+        "Unexpected error when retrieving manifests for built-in contracts",
+        e.message
+      );
+    } finally {
+      if (fs.existsSync(tempFile.path)) {
+        fs.unlinkSync(tempFile.path);
+      }
+    }
   }
 
   private async refreshLoop() {
@@ -63,7 +134,7 @@ export default class AutoComplete {
     const workspaceContracts = { ...this.contractDetector.contracts };
 
     const newData: AutoCompleteData = {
-      contractManifests: {},
+      contractManifests: { ...this.wellKnownManifests },
       contractHashes: {},
       contractPaths: {},
       contractNames: { ...WELL_KNOWN_NAMES },
