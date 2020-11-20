80a43b59af65a5b3639ec4a78afab8ffc1df55d1
David Nicholson <me@djnicholson.com>
2020-11-19 18:01:54 -0800

    allow comments everywhere we parse JSON

--- src/extension/JSONC.ts
+++ src/extension/JSONC.ts
@@ -1,7 +1,25 @@
+import * as jsoncParser from "jsonc-parser";
+
 export default class JSONC {
   static parse(input: string): any {
-    // TODO: Switch to using https://github.com/Microsoft/node-jsonc-parser
-    return JSON.parse(input);
+    const errors: jsoncParser.ParseError[] = [];
+    const result = jsoncParser.parse(input, errors, {
+      allowEmptyContent: true,
+      allowTrailingComma: true,
+      disallowComments: false,
+    });
+    if (errors.length) {
+      const elipsis = errors.length > 3;
+      errors.length = Math.min(errors.length, 3);
+      throw new Error(
+        `JSON parse error${errors.length > 1 ? "s" : ""} (${errors
+          .map(
+            (_) => `${jsoncParser.printParseErrorCode(_.error)} at ${_.offset}`
+          )
+          .join(", ")}${elipsis ? ", ..." : ""})`
+      );
+    }
+    return result;
   }
 
   static stringify(input: any) {
