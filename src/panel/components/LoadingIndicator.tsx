fc76e978cfbd6aac7bd33a7c791c89434ebc9e4b
David Nicholson <me@djnicholson.com>
2021-02-01 17:55:38 -0800

    use an animated spinner for the loading indicator

--- src/panel/components/LoadingIndicator.tsx
+++ src/panel/components/LoadingIndicator.tsx
@@ -1,26 +1,58 @@
 import React from "react";
 
 export default function LoadingIndicator() {
-  // TODO: Replace with a spinner (the word "loading" does not make sense in all contexts,
-  // e.g. when awaiting user input in the extension process)
+  // Inspiration for CSS-only loading spinner from: https://loading.io/css/
+
+  const outerStyle: React.CSSProperties = {
+    display: "inline-block",
+    position: "relative",
+    width: 40,
+    height: 40,
+  };
+
+  const innerStyle: React.CSSProperties = {
+    boxSizing: "border-box",
+    display: "block",
+    position: "absolute",
+    width: 32,
+    height: 32,
+    margin: 4,
+    border: "4px solid",
+    borderRadius: "50%",
+    animation: "lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite",
+    borderColor:
+      "var(--vscode-statusBar-background) transparent transparent transparent",
+  };
+
   return (
     <div
       style={{
         position: "fixed",
         bottom: 0,
         right: 0,
-        margin: 10,
-        paddingTop: 5,
-        paddingBottom: 5,
-        paddingLeft: 30,
-        paddingRight: 30,
-        backgroundColor: "var(--vscode-statusBar-background)",
-        color: "var(--vscode-statusBar-foreground)",
-        fontWeight: "bold",
+        marginBottom: 20,
+        marginRight: 40,
         zIndex: 10000,
       }}
     >
-      LOADING&hellip;
+      <style type="text/css">
+        {`
+          @keyframes lds-ring {
+            0% {
+              transform: rotate(0deg);
+            }
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+        `}
+      </style>
+      <div style={outerStyle}>
+        <div style={{ ...innerStyle, animationDelay: "-0.45s" }}></div>
+        <div style={{ ...innerStyle, animationDelay: "-0.3s" }}></div>
+        <div style={{ ...innerStyle, animationDelay: "-0.15s" }}></div>
+        <div style={{ ...innerStyle }}></div>
+      </div>
     </div>
   );
 }
