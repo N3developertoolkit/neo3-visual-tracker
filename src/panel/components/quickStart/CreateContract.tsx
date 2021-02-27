ee5ef515c46c7d10d525c31c0b26cb00849fe4de
David Nicholson <me@djnicholson.com>
2021-02-26 20:18:05 -0800

    don't reference "C#" in Quick Start (see #33)

--- src/panel/components/quickStart/CreateContract.tsx
+++ src/panel/components/quickStart/CreateContract.tsx
@@ -12,8 +12,8 @@ export default function CreateContract({ onCreate }: Props) {
       <div style={{ margin: 10, textAlign: "left" }}>
         You don't appear to have any smart contracts in the current Visual
         Studio Code worskspace. Would you like to create a new smart contract?
-        You can write your smart contract's code in C# and then deploy and test
-        it locally using Neo Express.
+        You can write your smart contract's code in a programming language of
+        your choice and then deploy and test it locally using Neo Express.
       </div>
       <NavButton style={{ margin: 10 }} onClick={onCreate}>
         Create a new contract
