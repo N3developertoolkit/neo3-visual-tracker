6ca91768d1d5ce2ab078c0de2defc5809597fe36
David Nicholson <me@djnicholson.com>
2021-04-19 17:51:32 -0700

    rc1 compatible sample workspace

---
+++ sample-workspaces/sample-workspace/Sample/SampleContract.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+
+namespace Sample
+{
+    [DisplayName("YourName.SampleContract")]
+    [ManifestExtra("Author", "Your name")]
+    [ManifestExtra("Email", "your@address.invalid")]
+    [ManifestExtra("Description", "Describe your contract...")]
+    public class SampleContract : SmartContract
+    {
+        const string MAP_NAME = "SampleContract";
+
+        [DisplayName("NumberChanged")]
+        public static event Action<Neo.UInt160, BigInteger> OnNumberChanged;
+
+        public static bool ChangeNumber(BigInteger positiveNumber)
+        {
+            if (positiveNumber < 0)
+            {
+                throw new Exception("Only positive numbers are allowed.");
+            }
+
+            var tx = (Transaction) ExecutionEngine.ScriptContainer;
+
+            var storageMap = Storage.CurrentContext.CreateMap(MAP_NAME);
+
+            storageMap.Put(tx.Sender, positiveNumber);
+
+            OnNumberChanged(tx.Sender, positiveNumber);
+
+            return true;
+        }
+
+        public static ByteString GetNumber()
+        {
+            var tx = (Transaction) ExecutionEngine.ScriptContainer;
+
+            var storageMap = Storage.CurrentContext.CreateMap(MAP_NAME);
+
+            return storageMap.Get(tx.Sender);
+        }
+    }
+}
